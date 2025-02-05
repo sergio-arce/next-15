62ed2953ce0cca404a4aad233632587f7bd9cd2c
Sergio Arce <arcesech30@gmail.com>
2025-02-05 17:45:31 +0100

    add templates in (auth) rute

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,8 +1,15 @@
+/**
+ * Diferencias clave entre layout.tsx y template.tsx
+ * - layout.tsx cuando quieres compartir UI y mantener estado entre páginas.
+ * - template.tsx cuando necesitas reiniciar estado en cada navegación. 
+ */
+
 'use client' 
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import "./styles.css"
+import { useState } from "react"
 
 const navLinks = [
   { name: "Register", href: '/register' },
@@ -12,10 +19,14 @@ const navLinks = [
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 
+  const [input, setInput] = useState("");
+  
+
   const pathname = usePathname()
   
   return (
     <div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
      { navLinks.map((link) => {
         const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/")
         return (
---
+++ src/app/(auth)/template.tsx
@@ -0,0 +1,41 @@
+/**
+ * Diferencias clave entre layout.tsx y template.tsx
+ * - layout.tsx cuando quieres compartir UI y mantener estado entre páginas.
+ * - template.tsx cuando necesitas reiniciar estado en cada navegación. 
+ */
+
+'use client' 
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import "./styles.css"
+import { useState } from "react"
+
+const navLinks = [
+  { name: "Register", href: '/register' },
+  { name: "Login", href: '/login' },
+  { name: "Forgot Password", href: '/forgot-password' },
+]
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+
+  const [input, setInput] = useState("");
+  
+
+  const pathname = usePathname()
+  
+  return (
+    <div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+     { navLinks.map((link) => {
+        const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/")
+        return (
+          <Link href={link.href} key={link.name} className={isActive ? "font-bold text-pink-600 mr-2" : "text-blue-600 mr-2"}>
+            { link.name }
+          </Link>
+        )
+     })}  
+     { children }
+    </div>
+  )
+}
