23a95ec7479faaa0c7b3b06a2fa58e6caeb2e2e2
Sergio Arce <arcesech30@gmail.com>
2025-02-05 11:41:02 +0100

    link-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
+
 export default function Home() {
-  return <div>Home page</div>
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Link href="/blog">Link to blog</Link>
+      <Link href="/product">Link to products</Link>
+    </div>
+  )
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,3 +1,27 @@
-export default function Product() {
-  return <div>Product</div>;
+import Link from "next/link";
+
+export default function ProductList() {
+  const productId = 10;
+
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>Product List</h1>
+      <h2>
+        <Link href="product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="product/2">Product 2</Link>
+      </h2>
+      <h2>
+        {/* replace sirve para volver a la raiz desde el navegador */}
+        <Link href="product/3" replace> 
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`product/${productId}`}>Product {productId}</Link>
+      </h2>
+    </>
+  );
 }
